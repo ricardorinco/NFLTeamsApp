c280ea53470aa19015db74d6107a7d496e636d16
Ricardo Rinco <ricardo.rinco@hotmail.com>
2017-06-01 00:42:02 -0300

    Solution creation

---
+++ NFLTeamsApp/NFLTeamsApp/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace NFLTeamsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
