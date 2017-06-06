d80e750bcd80d662bbe236b51c0cf3f7ade762bc
Ricardo Rinco <ricardo.rinco@hotmail.com>
2017-06-06 16:20:23 -0300

    Add PushNotification

--- NFLTeamsApp/NFLTeamsApp.Android/MainActivity.cs
+++ NFLTeamsApp/NFLTeamsApp.Android/MainActivity.cs
@@ -1,14 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Gcm.Client;
+using NFLTeamsApp.Droid.Services;
+using System;
 
 namespace NFLTeamsApp.Droid
 {
     [Activity(Label = "NFLTeamsApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        static MainActivity instance = null;
+        public static MainActivity CurrentActivity { get { return instance; } }
+
         protected override void OnCreate(Bundle bundle)
         {
+            instance = this;
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -16,6 +24,30 @@ namespace NFLTeamsApp.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new Views.App());
+
+            try
+            {
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the client. Verify the URL.", "Error");
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e.Message, "Error");
+            }
+        }
+
+        private void CreateAndShowDialog(String message, String title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message); builder.SetTitle(title); builder.Create().Show();
         }
     }
 }
---
+++ NFLTeamsApp/NFLTeamsApp/Helpers/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace NFLTeamsApp.Helpers
+{
+    public static class Constants
+    {
+        public static readonly string ApplicationURL = "https://nflteamsapp.azurewebsites.net/";
+    }
+}
